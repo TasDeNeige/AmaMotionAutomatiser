45045c7184dc21d05e0d4be341108b55785cff3d
Amaryne Breand <amaryne.breand@gmail.com>
2026-01-21 16:43:02 +0100

    Updated: Text Animations
    Changed how it's used

--- Components/AMA_Text.cs
+++ Components/AMA_Text.cs
@@ -7,31 +7,31 @@
 using AMA;
 using TMPro;
 using UnityEngine;
-using UnityEngine.XR;
+using System.Collections.Generic;
+using Unity.Plastic.Newtonsoft.Json;
 
 //[RequireComponent(typeof(TextMeshPro))]
 public class AMA_Text : MonoBehaviour
 {
-    // Tags
-    static string wavyOpenTag = "<wavy>";
-    static string wavyCloseTag = "</wavy>";
-    static string generalCloseTag = "</>";
+    #region Tag system
+    enum TagType { WAVY, FALLING_DOWN };
+    const string wavyTag = "wavy";
+    const string fallingDownTag = "falling_down";
 
+    struct TagInfo { public TagType tagType; public int startId; public int endId; }
+    List<TagInfo> tags = new List<TagInfo>();
+    #endregion
 
     TMP_Text textComponent;
-    int startAtId = 0;
-    int endAtId = int.MaxValue;
 
     [Header("Animation")]
     [SerializeField] bool skipInvisibleCharacters = true;
     [SerializeField] float waveSpeed = 2f;
     [SerializeField] float displacementIntensity = 0.01f;
-    [SerializeField] float waveIntensity  = 10.0f;
+    [SerializeField] float waveIntensity = 10.0f;
 
     private void Awake()
     {
-        //TMPro_EventManager.TEXT_CHANGED_EVENT.Add(TextChangedTier);
-
         // Retrieve text component
         textComponent = GetComponent<TMP_Text>();
 
@@ -46,119 +46,74 @@ public class AMA_Text : MonoBehaviour
         HandleTags();
     }
 
-    // Used to tie methods to TMP's TEXT_CHANGED event
-    private void TextChangedTier(Object obj)
-    {
-        HandleTags();
-    }
-
     private void HandleTags()
     {
-        // Wavy Open
-        if (textComponent.text.Contains(wavyOpenTag))
-        {
-            // Get start index
-            startAtId = textComponent.text.IndexOf(wavyOpenTag);
-            Debug.Log("Effect starts at " + startAtId);
-
-            // Erase tag from string
-            HideTag(wavyOpenTag, startAtId);
-        }
-
-        // Wavy Close
-        if (textComponent.text.Contains(wavyCloseTag))
-        {
-            // Get start index
-            endAtId = textComponent.text.IndexOf(wavyCloseTag);
-            Debug.Log("Effect ends at " + endAtId);
-
-            // Erase tag from string
-            HideTag(wavyCloseTag, endAtId);
-        }
-
-        // General Close
-        if (textComponent.text.Contains(generalCloseTag))
-        {
-            // Get start index
-            endAtId = textComponent.text.IndexOf(generalCloseTag);
-            Debug.Log("Effect ends at " + endAtId);
-
-            // Erase tag from string
-            HideTag(generalCloseTag, endAtId);
-        }
-    }
-
-    private void HideTag(string _tag, int _startId)
-    {
+        // Forced to get true Link count
         textComponent.ForceMeshUpdate();
-        TMP_TextInfo textInfo = textComponent.textInfo;
 
-        //textComponent.text = textComponent.text.Substring(0, _startId) + textComponent.text.Substring(_startId + _tag.Length);
+        // Process each tag
+        for (int i = 0; i < textComponent.textInfo.linkCount; i++)
+        {
+            TMP_LinkInfo link = textComponent.textInfo.linkInfo[i];
 
+            TagInfo newTag = new TagInfo();
+            newTag.startId = link.linkTextfirstCharacterIndex;
+            newTag.endId = newTag.startId + link.linkTextLength;
 
-        //for (int i = 0; i < _tag.Length; i++)
-        //{
-        //    int charIndex = _startId + i;
-        //    if (charIndex < 0 || charIndex >= textInfo.characterCount)
-        //        continue;
-
-        //    TMP_CharacterInfo charInfo = textInfo.characterInfo[charIndex];
-
-        //    if (!charInfo.isVisible)
-        //        continue;
-
-        //    int matIndex = charInfo.materialReferenceIndex;
-        //    int vertIndex = charInfo.vertexIndex;
-
-        //    Vector3[] verts = textInfo.meshInfo[matIndex].vertices;
-        //    Vector3 collapsed = verts[vertIndex];
-
-        //    verts[vertIndex + 0] = collapsed;
-        //    verts[vertIndex + 1] = collapsed;
-        //    verts[vertIndex + 2] = collapsed;
-        //    verts[vertIndex + 3] = collapsed;
-
-        //    textInfo.meshInfo[matIndex].mesh.vertices = verts;
-        //    textComponent.UpdateGeometry(textInfo.meshInfo[matIndex].mesh, matIndex);
-        //}
+            // If link is one of our tag
+            switch (link.GetLinkID())
+            {
+                case wavyTag: newTag.tagType = TagType.WAVY; tags.Add(newTag); break;
+                case fallingDownTag: newTag.tagType = TagType.FALLING_DOWN; tags.Add(newTag); break;
+                default: /* Not one of our tags */ break;
+            }
+        }
     }
-
 
     private void Update()
     {
+        // Don't process text if no tag has been registered
+        if (tags.Count <= 0) return;
+
         #region Animation
         textComponent.ForceMeshUpdate();
         TMP_TextInfo textInfo = textComponent.textInfo;
 
-        // Go through each character
-        for (int currentChar = startAtId;
-            currentChar < (endAtId < textInfo.characterCount ? endAtId : textInfo.characterCount);
-            ++currentChar)
+        // Go through each tag
+        for (int currentTag = 0; currentTag < tags.Count; currentTag++)
         {
-            TMP_CharacterInfo characterInfo = textInfo.characterInfo[currentChar];
-
-
-            // Skip invisible characters
-            if (skipInvisibleCharacters && !characterInfo.isVisible) { continue; }
-
-            Vector3[] verts = textInfo.meshInfo[characterInfo.materialReferenceIndex].vertices;
-            // Go through each verts
-            for (int currentVert = 0; currentVert < /*verts.Length*/ 4; ++currentVert)
+            // Go through each character
+            for (int currentChar = tags[currentTag].startId;
+                currentChar < (tags[currentTag].endId < textInfo.characterCount ? tags[currentTag].endId : textInfo.characterCount);
+                ++currentChar)
             {
-                // Current position of the vertex
-                Vector3 vertPos = verts[characterInfo.vertexIndex + currentVert];
+                TMP_CharacterInfo characterInfo = textInfo.characterInfo[currentChar];
 
-                // Animation code:
-                // Falling down text
-                //verts[characterInfo.vertexIndex + currentVert] = vertPos + new Vector3(0, Mathf.Atan(-(Time.time * 2f + -vertPos.x * 0.01f)) * 10f, 0);
+                // Skip invisible characters
+                if (skipInvisibleCharacters && !characterInfo.isVisible) { continue; }
 
-                // Wavy text
-                //verts[characterInfo.vertexIndex + currentVert] = vertPos + new Vector3(0, Mathf.Sin(-(Time.time * waveSpeed + -vertPos.x * displacementIntensity)) * waveIntensity, 0);
+                Vector3[] verts = textInfo.meshInfo[characterInfo.materialReferenceIndex].vertices;
 
-                verts[characterInfo.vertexIndex + currentVert] = Vector3.zero;
+                // Go through each verts
+                for (int currentVert = 0; currentVert < 4; ++currentVert)
+                {
+                    // Current position of the vertex
+                    Vector3 vertPos = verts[characterInfo.vertexIndex + currentVert];
 
-                // Push changes to the mesh
-                textComponent.UpdateVertexData(TMP_VertexDataUpdateFlags.Colors32);
+                    // Animation code:
+                    switch(tags[currentTag].tagType)
+                    {
+                        // Wavy text
+                        case TagType.WAVY:
+                            verts[characterInfo.vertexIndex + currentVert] = vertPos + new Vector3(0, Mathf.Sin(-(Time.time * waveSpeed + -vertPos.x * displacementIntensity)) * waveIntensity, 0);
+                            break;
+
+                        // Falling down text
+                        case TagType.FALLING_DOWN:
+                            verts[characterInfo.vertexIndex + currentVert] = vertPos + new Vector3(0, Mathf.Atan(-(Time.time * 2f + -vertPos.x * 0.01f)) * 10f, 0);
+                            break;
+                    }
+                }
             }
         }
 
